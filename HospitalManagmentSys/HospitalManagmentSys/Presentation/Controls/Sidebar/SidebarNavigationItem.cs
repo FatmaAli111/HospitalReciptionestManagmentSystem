c86eb84c62cc935b72fa74087547a27bedc996c4
Reem Abdelkader <reemabdelkader97@gmail.com>
2026-04-14 03:39:22 +0200

    Finalizing Reports Form and Donut Chart

---
+++ HospitalManagmentSys/HospitalManagmentSys/Presentation/Controls/Sidebar/SidebarNavigationItem.cs
@@ -0,0 +1,11 @@
+namespace HospitalManagmentSys.Presentation.Controls.Sidebar;
+
+public enum SidebarNavigationItem
+{
+    Dashboard,
+    Patients,
+    Appointments,
+    Queue,
+    Reports,
+    Settings
+}
